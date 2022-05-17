c8ff19fb20ee8aca9b7e4d4bdd14a1a5f9b57379
yurigarrido <y.garrido.yuri@icloud.com>
2022-05-17 13:18:14 -0300

    add seed user

---
+++ src/database/seeders/20220517161609-users.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: async (queryInterface) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkInsert('users', [
+      {
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com',
+        password: 'adminsecreto', // secret_admin
+      },
+      {
+        username: 'User',
+        role: 'user',
+        email: 'user@user.com',
+        password: 'usersecreto', // secret_user
+      },
+     
+    ], {});
+  },
+
+  down: async (queryInterface) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
