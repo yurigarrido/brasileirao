34b3c34571db8cf5c773f33ed5cf66ade29a93cc
yurigarrido <y.garrido.yuri@icloud.com>
2022-05-16 22:06:34 -0300

    add nodemon, ts, .env and init server

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import 'dotenv/config'
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`escutando na porta ${PORT}`)
+});
