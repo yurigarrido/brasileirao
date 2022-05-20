62b7736856efee948f8733db8593436fb4bc0456
yurigarrido <y.garrido.yuri@icloud.com>
2022-05-19 21:51:39 -0300

    fix backup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-const express = require('express');
-require('dotenv').config()
+import * as express from 'express';
+import 'dotenv/config'
+import login from './routers/login'
 
 const app = express();
 
+const accessControl: express.RequestHandler = (_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+};
+app.use(accessControl);
+
+app.use('/login', login)
 
 const PORT = process.env.PORT || 3000
 
