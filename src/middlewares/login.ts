093e310ed4c9139f26060cb0369e27774facc589
yurigarrido <y.garrido.yuri@gmail.com>
2022-05-26 19:19:54 -0300

    fix: add verificação no middleware de email e senha

--- src/middlewares/login.ts
+++ src/middlewares/login.ts
@@ -1,10 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import User from '../models/users';
 
-const email = (req: Request, res:Response, next:NextFunction) => {
+const email = async (req: Request, res:Response, next:NextFunction) => {
   const { email } = req.body;
+  const user = await User.findOne({ where: { email }, raw: true });
 
   if(!email) return res.status(401).json({ sucess: false, data: {}, token: null, message: 'email is required' });
+  if(!user) return res.status(401).json({ sucess: false, data: {}, token: null, message:  'Incorrect email or password'  });
+
 
   next();
 };
@@ -15,7 +18,7 @@ const password = async (req: Request, res:Response, next:NextFunction) => {
 
   if(!password) return res.status(401).json({ sucess: false, data: {}, token: null, message: 'password is required' });
   if(!user) return res.status(401).json({ sucess: false, data: {}, token: null, message: 'user not exist' });
-  if(user.password !== password) return res.status(401).json({ sucess: false, data: {}, token: null, message: 'not authorized' });
+  if(user.password !== password) return res.status(401).json({ sucess: false, data: {}, token: null, message:  'Incorrect email or password'  });
 
   next();
 };
